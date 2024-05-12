b2575f0f389408b9712b1b312ec0bc723f9d5305
Luisa738 <00001081649471sp@al.educacao.sp.gov.br>
2024-05-12 00:28:27 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("pink");
+    
+  stroke("purple") 
+    fill ("purple")  
+    
+    if(mouseIsPressed)
+    console.log(mouseIsPressed)
+    rect(mouseX, mouseY, 100, 150)
+  
+    
+  }
